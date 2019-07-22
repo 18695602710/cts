e021dc9d5a5b6b9d0dc1a87a38f96d025873c98b
chents <18695602710@163.com>
2019-07-22 15:48:06 +0800

    word

--- word.py
+++ word.py
@@ -8,10 +8,17 @@ import jieba
 d = path.dirname(__file__)
 text = open(path.join(d, 'cts.txt'),encoding='utf-8').read()
 # 结巴分词
+stopwords = [line.strip() for line in open(path.join(d, 'ban.txt'), 'r').readlines()]
 wordlist = jieba.cut(text)
-wl = " ".join(wordlist)
+#wl = " ".join(wordlist)
+outstr = ''
+for word in wordlist:
+    if word not in stopwords:
+        if word != '\t':
+            outstr += word
+            outstr += " "
 cloud_mask = np.array(Image.open(path.join(d, "1.png")))
-wordcloud = WordCloud(background_color="white",max_words=200,mask=cloud_mask,font_path='HYQiHeiY4-95W.otf').generate(text)
+wordcloud = WordCloud(background_color="white",max_words=200,mask=cloud_mask,font_path='HYQiHeiY4-95W.otf').generate(outstr)
 image_colors = ImageColorGenerator(cloud_mask)
 wordcloud.recolor(color_func=image_colors)
 plt.imshow(wordcloud, interpolation="bilinear")
